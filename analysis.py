8bd64a955e774d4fabc3a90348e288ef8364c8c2
Phạm Quốc Trung <73487076+thesunsavior@users.noreply.github.com>
2022-05-28 22:08:43 +0700

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -20,8 +20,8 @@
 # value iteration.
 
 def question2():
-    answerDiscount = 0.9
-    answerNoise = 0.2
+    answerDiscount = 0.2
+    answerNoise = 0.0
     return answerDiscount, answerNoise
 
 def question3a():
